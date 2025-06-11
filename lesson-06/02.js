856cdd88101dd8308d7d675fe063dee5dfb4b142
Alexandra Goncharova <alexandra.kazak333@gmail.com>
2025-06-11 23:37:59 +0300

    doing hw

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,11 +24,63 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = (array, callback) => {
-  const result = []
-  for (let index = 0; index < array.length; index++){
-    const element = array[index]
-    result.push(callback(element, index))
+// const map = (array, callback) => {
+//   const result = []
+//   for (let index = 0; index < array.length; index++){
+//     const element = array[index]
+//     result.push(callback(element, index))
+//   }
+//   return result
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Вот перевод вашего текста на русский язык:
+
+// Ваша задача — создать функцию, которая может принимать любое неотрицательное целое число в качестве аргумента 
+// и возвращать его с цифрами в порядке убывания.По сути, необходимо переставить цифры, чтобы создать наибольшее возможное число.
+
+//   Примеры:
+// Вход: 42145 Выход: 54421
+
+// Вход: 145263 Выход: 654321
+
+// Вход: 123456789 Выход: 987654321
+
+
+
+
+
+
+
+function descendingOrder(number) {
+  if (number >= 0) {
+    let string = number.toString()
+    let array = string.split('')
+    
+  
+    let newNumber = array.sort((prev, next) => next - prev) 
+
+   return +newNumber.join('')
+   
+  } else {
+    return null
   }
-  return result
 }
+
+console.log(descendingOrder(5423));
--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,3 +17,56 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const imgBlock = document.getElementById('web-tech-image')
+
+
+
+//предыдущая кнопка
+const capybara = document.getElementById('prev-button')
+capybara.addEventListener('click', function() {
+  // ищем текущую картинку
+  const img = imgBlock.getAttribute('src')
+  // перебираем все картинки
+  for (let i = 0; i < WEB_TECH_IMAGES.length; i++) {
+    // ищем совпадения
+    if (WEB_TECH_IMAGES[i] === img) {
+      // проверяем существует ли картинка предыдущая, если есть то переходим дальше...
+      if (WEB_TECH_IMAGES[i - 1]) {
+         // если совпало, переключаемся на предыдущую
+        imgBlock.setAttribute('src', WEB_TECH_IMAGES[i - 1])
+        } else {
+          imgBlock.setAttribute('src', WEB_TECH_IMAGES[WEB_TECH_IMAGES.length - 1])
+        }
+     
+    } 
+  }
+
+  
+})
+
+
+
+// следущая кнопка
+const kotya = document.getElementById('next-button')
+kotya.addEventListener('click', function () {
+  // ищем текущую картинку
+  const img = imgBlock.getAttribute('src')
+  // перебираем все картинки
+  for (let i = 0; i < WEB_TECH_IMAGES.length; i++ ) {
+    // ищем совпаления
+    if(WEB_TECH_IMAGES[i] === img) {
+    //  смотрим есть ли вообще след картинка
+      if(WEB_TECH_IMAGES[i+1]) {
+        // если совпало , то переключаемся на следующую
+        imgBlock.setAttribute('src', WEB_TECH_IMAGES[i + 1])
+      } else {
+        // если следующец кратинки  нет, мы переключимся на первую
+        imgBlock.setAttribute('src', WEB_TECH_IMAGES[0])
+      }
+      
+    }
+  }
+
+
+})
