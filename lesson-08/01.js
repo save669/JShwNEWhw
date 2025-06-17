66f94be126738b2510b2fe952a1c2daf94224fe0
Alexandra Goncharova <alexandra.kazak333@gmail.com>
2025-06-17 03:32:31 +0300

    вщштп рц

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,17 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+let result = ''
+for (let i = 0; i < str.length; i++){
+  if (i === 0 || str[i - 1] === ' ') {
+    result = result + str[i].toUpperCase()
+  } else {
+    result = result + str[i].toLowerCase()
+  }
+}
+return result
+}
+
+
+console.log(capitalizeWords("rbtbhrb  rb4gh4  hgb4bh btrbb4t5"));      
